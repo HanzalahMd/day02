a9c16f53b037f3b36a210510ed2d2a8daaa84ca2
HanzalahMd <hanzalahmohammed@gmail.com>
2020-10-27 16:37:46 +0800

    Modified

--- main.js
+++ main.js
@@ -1,21 +1,13 @@
 const { text } = require('express');
 const express = require('express');
-const hbs = require('express-handlebars');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', hbs({defaultlayout: 'main.hbs'}));
+app.engine('hbs', handlebars({defaultLayout: 'main.hbs'}));
 app.set('view engine', 'hbs');
 
 const PORT = parseInt(process.argv[2]) || 4400;
-
-app.listen(PORT, () => {
-    console.log (`The application has started on ${PORT}: ${new Date}`);
-})
-
-app.use(
-    express.static(__dirname + '/public')
-)
 
 app.get('/result', (req, res) => {
     res.status(200);
@@ -29,4 +21,10 @@ app.get('/test', (req, res) => {
     res.render('test');
 })
 
-console.log('modified');
+app.use(
+    express.static(__dirname + '/public')
+)
+
+app.listen(PORT, () => {
+    console.log (`The application has started on ${PORT}: ${new Date}`);
+})
