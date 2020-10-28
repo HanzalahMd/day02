47a68af27b3e859d9eabfc2a6eae6ce9c6205747
HanzalahMd <hanzalahmohammed@gmail.com>
2020-10-28 10:05:43 +0800

    completed

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 const { text } = require('express');
 const express = require('express');
-const handlebars = require('express-handlebars');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', handlebars({defaultLayout: 'main.hbs'}));
+app.engine('hbs', hbs({defaultLayout: 'default.hbs'}));
 app.set('view engine', 'hbs');
 
 const PORT = parseInt(process.argv[2]) || 4400;
+
+const rollDices = () => Math.floor(Math.random() * 6 + 1);
+
+const diceImages = [
+    '', 'one.png', 'two.png', 'three.png', 'four.png', 'five.png', 'six.png'
+]
+
+app.get('/roll', (req, res) => {
+    const d1 = diceImages[rollDices()];
+    const d2 = diceImages[rollDices()];
+    res.status(200);
+    res.type('text/html');
+    res.render('roll', {d1, d2});
+})
 
 app.get('/result', (req, res) => {
     res.status(200);
@@ -21,6 +35,12 @@ app.get('/test', (req, res) => {
     res.render('test');
 })
 
+app.get('/', (req, res) => {
+    res.status(200);
+    res.type('text/html');
+    res.render('index');
+})
+
 app.use(
     express.static(__dirname + '/public')
 )
@@ -28,3 +48,4 @@ app.use(
 app.listen(PORT, () => {
     console.log (`The application has started on ${PORT}: ${new Date}`);
 })
+
